bfb47b0c38fc678add181596f81d4f273d65cd15
feromant <maznichenko@list.ru>
2021-10-01 21:18:44 +0300

    test passed

--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -1,9 +1,20 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Egg6 {
+
+    private Needle7 niddle;
+
+    @Autowired
+    public Egg6(Needle7 niddle) {
+        this.niddle = niddle;
+    }
 
     @Override
     public String toString() {
-        return ", в яйце иголка " + "";
+        return ", в яйце иголка " + niddle.toString();
     }
 }
